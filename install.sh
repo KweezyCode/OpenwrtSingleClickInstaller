5dd8305c60ad6b1848fecea038c5aa2fcc3fa3e6
KweezyCode <unknown2w+github@gmail.com>
2025-05-16 00:49:42 +0300

    Add echo statements for better user feedback during installation process

--- install.sh
+++ install.sh
@@ -2,15 +2,18 @@
 set -e
 
 # Обновляем apk и ставим необходимые утилиты
+echo "Установка unzip..."
 apk update
 apk add --no-cache unzip
 
 # Скачиваем и распаковываем репозиторий
+echo "Скачивание и распаковка..."
 cd /tmp
 wget -qO main.zip https://github.com/KweezyCode/OpenwrtSingleClickInstaller/archive/refs/heads/main.zip
 unzip -o main.zip
 
 # Запускаем оригинальный инсталлятор
+echo "Распаковка завершена. Запуск инсталлятора..."
 cd OpenwrtSingleClickInstaller-main
 chmod +x main.sh
 ./main.sh
